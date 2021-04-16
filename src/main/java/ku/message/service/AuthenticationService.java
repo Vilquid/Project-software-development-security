0da9b5c186060bc6d728ea55a0467380a2eb7b5e
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-04-16 17:47:04 +0700

    Logging
    Specify log level
    Add config to application.properties
    Modification of .gitignore to don't send the logs

--- src/main/java/ku/message/service/AuthenticationService.java
+++ src/main/java/ku/message/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import ku.message.model.User;
 import ku.message.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -15,34 +16,34 @@ import java.util.ArrayList;
 @Service
 public class AuthenticationService implements AuthenticationProvider
 {
-    @Autowired
-    private UserRepository repository;
+	@Autowired
+	private UserRepository repository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException
-    {
-        String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException
+	{
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
 
-        User user = repository.findByUsername(username);
+		User user = repository.findByUsername(username);
 
-        if (user != null)
-        {
-            if (passwordEncoder.matches(password, user.getPassword()))
-            {
-                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
-            }
-        }
+		if (user != null)
+		{
+			if (passwordEncoder.matches(password, user.getPassword()))
+			{
+				return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+			}
+		}
 
-        return null;
-    }
+		throw new BadCredentialsException("Authentication failed");
+	}
 
-    @Override
-    public boolean supports(Class<?> authentication)
-    {
-        return authentication.equals(UsernamePasswordAuthenticationToken.class);
-    }
+	@Override
+	public boolean supports(Class<?> authentication)
+	{
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
 }
